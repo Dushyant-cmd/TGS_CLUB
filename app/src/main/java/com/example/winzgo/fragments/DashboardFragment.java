746b98d8f9f41861e0a8ae160896bdab895e16dd
dushyant <dushyant.977360@gmail.com>
2025-12-07 21:36:33 +0530

    app done

--- app/src/main/java/com/example/winzgo/fragments/DashboardFragment.java
+++ app/src/main/java/com/example/winzgo/fragments/DashboardFragment.java
@@ -7,14 +7,12 @@ import static com.example.winzgo.utils.Constants.setDarkMode;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatDelegate;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 
 import com.example.winzgo.BuildConfig;
 import com.example.winzgo.MainActivity;
@@ -32,13 +30,13 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class DashboardFragment extends Fragment {
     private FragmentDashboardBinding binding;
     private MainActivity activity;
     private FirebaseFirestore firestore;
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -157,9 +155,7 @@ public class DashboardFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-        if(!BuildConfig.DEBUG) {
-            getIsUpdate();
-        }
+        getIsUpdate();
     }
 
     private void getIsUpdate() {
